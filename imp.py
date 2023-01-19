8dedd096e6d0b375eb25085edac8218fefeaff82
Captainsteve1 <108216191+Captainsteve1@users.noreply.github.com>
2023-01-19 22:31:36 +0530

    Update imp.py

--- imp.py
+++ imp.py
@@ -1,12 +1,11 @@
 from links import *
 import os
-
 import subprocess
 from urllib.request import *
 from hachoir.parser import createParser
 from datetime import datetime
 
-def multirec(client, message, channel, url, title, lang, s, s2)
+def multirec(client, message, channel, url, title, lang, s, s2):
     f1 = 'ffmpeg -y -i'
     f2 = f'{url} -ss {s} -to {s2}'
     f3 = '-map 0:v:0 -map 0:a -metadata:s:a title=f'{tn}' -c copy'
@@ -14,5 +13,4 @@ def multirec(client, message, channel, url, title, lang, s, s2)
     merge = f1.split() + [url] + f2.split() + f3.split() + [output]
 
 subprocess.run(merge)
-
 return output 
