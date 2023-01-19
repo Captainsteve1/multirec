bf52aadfcda9ad3bb8ff66ed3796b17126add727
Captainsteve1 <108216191+Captainsteve1@users.noreply.github.com>
2023-01-19 22:08:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ from pyrogram.types import *
 from urllib.request import urlopen, Request 
 from rec import *
 
+newfile = "hi"
 
 tbot = Client("IPTV recorder bot for kids", bot_token=config.bot_token, api_id=config.api_id, api_hash=config.api_hash)
 
 @tbot.on_message(filters.command('rec') & filters.private)
 def rip(bot, update):
 
-newfile = "hi test"
+message.update.replay_text(text="recording")
 
   if os.path.exists(newfile) == True:
 
