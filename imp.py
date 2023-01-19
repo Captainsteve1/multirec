6b8d22fd1a0acee29a3dc9566967232bfb6de1b7
Captainsteve1 <108216191+Captainsteve1@users.noreply.github.com>
2023-01-19 21:47:09 +0530

    Update imp.py

--- imp.py
+++ imp.py
@@ -6,10 +6,10 @@ from urllib.request import *
 from hachoir.parser import createParser
 from datetime import datetime
 
-def multirec(client, message, channel, url, title, lang)
+def multirec(client, message, channel, url, title, lang, s, s2)
     f1 = 'ffmpeg -y -i'
     f2 = f'{url} -ss {s} -to {s2}'
-    f3 = '-map 0:v:0 -map 0:a -metadata:s:a title=f'tn' -c copy'
+    f3 = '-map 0:v:0 -map 0:a -metadata:s:a title=f'{tn}' -c copy'
     output = f'{title} {channel} [{lang}] [H264]_Tony.mkv'
     merge = f1.split() + [url] + f2.split() + f3.split() + [output]
 
