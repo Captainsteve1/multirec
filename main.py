3280da5454267f7118c7fecc83651e294fdebebf
Captainsteve1 <108216191+Captainsteve1@users.noreply.github.com>
2023-01-19 22:11:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,8 +13,9 @@ tbot = Client("IPTV recorder bot for kids", bot_token=config.bot_token, api_id=c
 @tbot.on_message(filters.command('rec') & filters.private)
 def rip(bot, update):
     message.update.replay_text(text="recording")
-    if os.path.exists(newfile) == True:
-    msg.edit(text="Recording done")
-    bot.send_video(video=newfile, chat_id=update.from_user.id, caption = f'{newfile}'
+
+if os.path.exists(newfile) == True:
+   msg.edit(text="Recording done")
+   bot.send_video(video=newfile, chat_id=update.from_user.id, caption = f'{newfile}'
 
 tbot.run()
