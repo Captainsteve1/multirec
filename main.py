93b0b55a7d1c600d13d023f5eddd5190b50899d9
Captainsteve1 <108216191+Captainsteve1@users.noreply.github.com>
2023-01-19 22:50:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ newfile = "hi.mkv"
 tbot = Client("IPTV recorder bot for kids", bot_token=config.bot_token, api_id=config.api_id, api_hash=config.api_hash)
 
 @tbot.on_message(filters.command('rec') & filters.private)
-def rip(bot, update, message):
+def rip(update, message):
     message.update.replay_text(text="recording")
 
 if os.path.exists(newfile) == True:
