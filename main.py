4e0d8fad2f0a84ce772a536ebac0c53b85e39026
Captainsteve1 <108216191+Captainsteve1@users.noreply.github.com>
2023-01-20 22:24:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ from pyrogram.types import *
 from urllib.request import urlopen, Request 
 from imp import *
 
-newfile = "hi.mkv"
-
 tbot = Client("IPTV recorder bot for kids", bot_token=config.bot_token, api_id=config.api_id, api_hash=config.api_hash)
 
 @tbot.on_message(filters.command('rec') & filters.private)
 def multirec(client, message):
     message.reply_text(text="recording")
+
+newfile = multirec(client, message, channel, url, title, lang, s, s2)
 
 if os.path.exists(newfile) == True:
    msg.edit(text="Recording done")
