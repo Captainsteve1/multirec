b5b7f109f02a157e931f49c16a45ca8bf092b82d
Captainsteve1 <108216191+Captainsteve1@users.noreply.github.com>
2023-01-20 22:30:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ from imp import *
 tbot = Client("IPTV recorder bot for kids", bot_token=config.bot_token, api_id=config.api_id, api_hash=config.api_hash)
 
 @tbot.on_message(filters.command('rec') & filters.private)
-def multirec(client, message):
+def mutirec(client, message):
+    multirec(client, message)
     message.reply_text(text="recording")
 
 newfile = multirec(client, Message)
